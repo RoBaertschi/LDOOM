a01f50f477fab55250412c6e242e611ef8a3ff44
RoBaertschi <rtmbaertschi007@gmail.com>
2025-10-25 20:41:33 +0200

    r_data.c: fix wrong pointer to int cast and declare suspicious unused variable as unused

--- linuxdoom/r_data.c
+++ linuxdoom/r_data.c
@@ -194,6 +194,7 @@ R_DrawColumnInCache
     byte*	dest;
 	
     dest = (byte *)cache + 3;
+    (void)dest; // TODO(robin): LDOOM: should this have been used and is now a bug?
 	
     while (patch->topdelta != 0xff)
     {
@@ -639,7 +640,7 @@ void R_InitColormaps (void)
     lump = W_GetNumForName("COLORMAP"); 
     length = W_LumpLength (lump) + 255; 
     colormaps = Z_Malloc (length, PU_STATIC, 0); 
-    colormaps = (byte *)( ((int)colormaps + 255)&~0xff); 
+    colormaps = (byte *)( ((uintptr)colormaps + 255)&~0xff); 
     W_ReadLump (lump,colormaps); 
 }
 
